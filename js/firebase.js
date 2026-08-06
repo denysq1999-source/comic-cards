63d6b57c67dde0718f9afcd4e1b7bf14c1c4bb3f
denysq1999-source <denysq1999@gmail.com>
2026-08-06 23:42:25 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,4 @@
+
 // Firebase SDK (модульна версія)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
