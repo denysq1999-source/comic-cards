d448c5c4d46842a0af7c1625493b9ef342383366
denysq1999-source <denysq1999@gmail.com>
2026-08-06 23:44:07 +0300

    Remove empty line at the beginning of firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,3 @@
-
 // Firebase SDK (модульна версія)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
